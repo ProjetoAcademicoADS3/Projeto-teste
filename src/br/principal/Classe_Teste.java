471b31bd42a314c4fcb8627c5f82c0b65e93562b
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr46@gmail.com>
2020-10-29 16:48:41 -0300

    Primeiro commit para teste

---
+++ src/br/principal/Classe_Teste.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.principal;
+
+/**
+ *
+ * @author roger
+ */
+public class Classe_Teste {
+    public static void main(String[] args) {
+        
+        int a = 9;
+        int b = 9;
+        int resultado = a + b;
+        
+        System.out.println(resultado);
+    }
+    
+}
